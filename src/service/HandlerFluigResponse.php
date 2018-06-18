f1ac8fec0073e2c8b6541023f0afde46c0915318
pedro <pedro.lazari@adias.com.br>
2018-06-18 11:52:44 -0300

    Criando novo response erro

--- src/service/HandlerFluigResponse.php
+++ src/service/HandlerFluigResponse.php
@@ -9,4 +9,15 @@ class HandlerFluigResponse
         $data['content'] = json_decode($res->getBody()->getContents(), true);
         return json_encode($data);
     }
+
+    /**
+     * @param $res
+     * @return statusCode = 500, validar o erro
+     *
+     */
+    public function parseResponseError($res){
+        $data = json_decode($res->getResponse()->getBody()->getContents(), true);
+        $data['statusCode'] = $res->getCode();
+        return json_encode($data);
+    }
 }
